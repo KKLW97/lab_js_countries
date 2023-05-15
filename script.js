53273db7f7613eba4c11d08957385aa2effa574f
Kelly Wong <kellykl_wong@hotmail.com>
2023-05-15 17:00:28 +0100

    adds mvp 2

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ const fetchCountries =  async () => {
     const countryResponse = await fetch("https://restcountries.com/v3.1/all");
     const countryJsonData = await countryResponse.json();
     console.log(countryJsonData);
-
 };
-fetchCountries();
+// fetchCountries();
 
 // mvp 2
-// 
+// create function setup()
+// have it call first function and populate a global variable with the output
+// call it when the page loads
+const setup = async () => {
+    const countries = fetchCountries();
+}
+
+window.addEventListener("load", setup);
+
+// mvp 3 
+// create a function that uses the global variable to create a series of new HTML elements
+// populating each with information about each country (such as name and population)
+// adding them to the <ul> in HTML
+// Call this function following first function within the SetUp() function
+// Ensure that the original <p> element is removed ahead of populating your list
 
 
 
