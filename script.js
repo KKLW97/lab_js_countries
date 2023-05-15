77cbbf8359214587d2f0ea9ac7327647e85798fc
Kelly Wong <kellykl_wong@hotmail.com>
2023-05-15 15:23:34 +0100

    adds mvp 1

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+// mvp 1
+const fetchCountries =  async () => {
+    const countryResponse = await fetch("https://restcountries.com/v3.1/all");
+    const countryJsonData = await countryResponse.json();
+    console.log(countryJsonData);
+
+};
+fetchCountries();
+
+// mvp 2
+// 
+
+
+
 // loading multiple results
 // on button click = fetch country data
 // have a section with id countries container 
 // loop through each country
-// create a li element setting the .url property to be the actual URL
+// create a li element 
 // append each li element to the section? or to the ul id countriesList?
 
-const fetchCountries = async () => {
-    const response = await fetch("https://restcountries.com/v3.1/all");
-    const data = await response.json();
+// const fetchCountries = async () => {
+//     const countryResponse = await fetch("https://restcountries.com/v3.1/all?fields=name");
+//     const countryJsonData = await countryResponse.json();
 
-}
+//     const countryList = document.querySelector("#countriesList");
+
+//     countryJsonData.name.forEach((name) => {
+//         const countryLi = document.createElement("li");
+//         countryLi.innerText = name.common;
+//         countryList.appendChild(countryLi);
+//     });
+
+// }
+
