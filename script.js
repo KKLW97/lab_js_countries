a20ce880e921daf6930f68d84fd3ab69dca1eb8a
Kelly Wong <kellykl_wong@hotmail.com>
2023-05-15 19:02:57 +0100

    adds mvp 5

--- script.js
+++ script.js
@@ -10,9 +10,12 @@ const fetchCountries =  async () => {
 // create function setup()
 // have it call first function and populate a global variable with the output
 // call it when the page loads
+let countries = "";
+let countriesList = "";
+
 const setup = async () => {
-    const countries = await fetchCountries();
-    const countriesList = await loadCountryList();
+    countries = await fetchCountries();
+    countriesList = await loadCountryList();
 }
 
 window.addEventListener("load", setup);
@@ -41,17 +44,71 @@ const loadCountryList = async () => {
         countryLi.innerText =url.name.common + " " + url.flag + " " + url.population;
         countryList.appendChild(countryLi);
     });
+
+    
+    const submitButton = document.querySelector("#submit");
+
+    const filterByInput = function() {
+        // const filteredCountriesList = document.createElement("ul")
+        // document.querySelector("#countriesList").remove();
+        const inputValue = document.querySelector("#search_input").value.toLowerCase();
+        for(i=0; i<countryJsonData.length; i++){
+            if(countryJsonData[i].name.common.toLowerCase().includes(inputValue)){
+                const filteredResult = document.createElement("li");
+                filteredResult.innerText = countryJsonData[i].name.common + " " + countryJsonData[i].flag + " " + countryJsonData[i].population;
+
+                countryList.appendChild(filteredResult);
+            };
+        };
+        
+        // document.querySelector("#countriesList").remove();
+
+
+    };
+    submitButton.addEventListener("click", event => {
+        countryList.innerHTML = "";
+        filterByInput();
+    });
+
 }
+
 
 // mvp 4
 // Add a simple <form> to your HTML with a single text-box input and a submit button
 // Create a function which is called when the form is submitted
 // printing the value of the <input> element to the console
-const submitButton = document.querySelector("#submit");
+// const submitButton = document.querySelector("#submit");
 
-const newSearch = async () => {
-    const inputValue = document.querySelector("#search_input").value;
-    console.log(inputValue);
-}
+// const newSearch = async () => {
+//     inputValue = document.querySelector("#search_input").value;
+//     console.log(inputValue);
+// };
 
-submitButton.addEventListener("click", () => newSearch());
+// submitButton.addEventListener("click", () => newSearch());
+
+// mvp 5
+// Create a function which takes your global variable and filters it based off of the value received
+// from your <form> above. Replace the contents of your <ul> with the filtered countries returned
+// const submitButton = document.querySelector("#submit");
+
+//     const filterByInput = function() {
+//         // const filteredCountriesList = document.createElement("ul")
+//         // document.querySelector("#countriesList").remove();
+//         const inputValue = document.querySelector("#search_input").value.toLowerCase();
+//         for(i=0; i<countryJsonData.length; i++){
+//             if(countryJsonData[i].name.common.toLowerCase().includes(inputValue)){
+//                 const filteredResult = document.createElement("li");
+//                 filteredResult.innerText = countryJsonData[i].name.common + " " + countryJsonData[i].flag + " " + countryJsonData[i].population;
+
+//                 countryList.appendChild(filteredResult);
+//             };
+//         };
+        
+//         // document.querySelector("#countriesList").remove();
+
+
+//     };
+//     submitButton.addEventListener("click", event => {
+//         countryList.innerHTML = "";
+//         filterByInput();
+//     });
