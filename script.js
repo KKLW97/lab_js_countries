ccba90685ac08b3e18f72a4c6f674782e836a1c3
Kelly Wong <kellykl_wong@hotmail.com>
2023-05-15 14:51:52 +0100

    adds psuedo code for mvp 1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Testing, testing, 1 2 3 ...")
+// loading multiple results
+// on button click = fetch country data
+// have a section with id countries container 
+// loop through each country
+// create a li element setting the .url property to be the actual URL
+// append each li element to the section? or to the ul id countriesList?
+
+const fetchCountries = async () => {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
+
+}
